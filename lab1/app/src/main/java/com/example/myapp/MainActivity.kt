33ffc6bdf37f51fe0af0da47be3ec8a83a66c343
bozdel <bondarevv.slavavv@gmail.com>
2024-12-27 00:38:43 +0700

    added back button handling

--- lab1/app/src/main/java/com/example/myapp/MainActivity.kt
+++ lab1/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             val nextPage = Intent(this, MainActivity2::class.java)
             startActivity(nextPage)
         }
+        val callback = onBackPressedDispatcher.addCallback(this) {
+            val str = " onBackPressedCallback fired"
+            val textview: TextView = findViewById(R.id.textView3)
+            textview.append(str)
+            finish()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        val str = " onNewIntent called"
+        val textview: TextView = findViewById(R.id.textView3)
+        textview.append(str)
     }
 }
